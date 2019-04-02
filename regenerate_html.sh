fced5c491c4db742c269887708d05dea4a4532ed
Connor Turland <connorturland@gmail.com>
2019-04-02 17:21:34 -0400

    0.0.9 basics

--- regenerate_html.sh
+++ regenerate_html.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export HC_VERSION=v0.0.8-alpha
-export HC_VERSION_FOR_URL=0.0.8-alpha
+export HC_VERSION=v0.0.9-alpha
+export HC_VERSION_FOR_URL=0.0.9-alpha
 export HC_RUST_VERSION=2019-01-24
-./docs
+./regenerate_html_bin
