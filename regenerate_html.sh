6652b245c026696005a8a8ac552b6b63d6aa47ad
Connor Turland <connorturland@gmail.com>
2019-04-10 09:35:51 -0400

    0.0.10-alpha2 release

--- regenerate_html.sh
+++ regenerate_html.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export HC_VERSION=v0.0.9-alpha
-export HC_VERSION_FOR_URL=0.0.9-alpha
+export HC_VERSION=v0.0.10-alpha2
+export HC_VERSION_FOR_URL=0.0.10-alpha2
 export HC_RUST_VERSION=2019-01-24
 ./regenerate_html_bin
