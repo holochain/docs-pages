841e747d5cf65ddc1c49245011bfd2a84ad86421
Connor Turland <connorturland@gmail.com>
2019-05-01 14:00:47 -0400

    update latest

--- api/latest/holochain_core/nucleus/ribosome/api/commit/sidebar-items.js
+++ api/latest/holochain_core/nucleus/ribosome/api/commit/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["invoke_commit_app_entry","ZomeApiFunction::CommitAppEntry function code args: [0] encoded MemoryAllocation as u64 Expected complex argument: CommitArgs Returns an HcApiReturnCode as I64"]]});
+initSidebarItems({"fn":[["invoke_commit_app_entry","ZomeApiFunction::CommitAppEntry function code args: [0] encoded MemoryAllocation as u64 Expected complex argument: CommitEntryArg Returns an HcApiReturnCode as I64"]]});
