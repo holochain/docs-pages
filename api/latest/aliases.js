18a9a3f0ccc7784c8b8e55d62adb0b8e3ec87821
Connor Turland <connorturland@gmail.com>
2019-06-20 18:18:17 -0400

    update to latest

--- api/latest/aliases.js
+++ api/latest/aliases.js
@@ -1,10 +1,9 @@
 var ALIASES = {};
 ALIASES["hdk"] = {};
-ALIASES["holochain_cas_implementations"] = {};
 ALIASES["holochain_common"] = {};
 ALIASES["holochain_conductor_wasm"] = {};
 ALIASES["holochain_core_types"] = {};
-ALIASES["holochain_core_types_derive"] = {};
 ALIASES["holochain_dna_c_binding"] = {};
 ALIASES["holochain_dpki"] = {};
+ALIASES["holochain_net"] = {};
 ALIASES["holochain_wasm_utils"] = {};
