5c8b5fb1bc5fbf06830b6b5dd20a4164ac22668c
Tom Gowan <s3549186@student.rmit.edu.au>
2019-10-28 12:46:27 +1100

    ignore docs

--- dev_build.sh
+++ dev_build.sh
@@ -14,10 +14,10 @@ rebuild_on_update() {
 	run_on_update docs/ "-I{} mkdocs build -d build/docs"
 }
 
-run_on_update coreconcepts/tutorials ./cc_tuts_build.sh
-run_on_update coreconcepts/ ./dev_build_art.sh
+run_on_update src/tutorials/coreconcepts/ ./cc_tuts_build.sh
+run_on_update src/concepts ./dev_build_art.sh
 run_on_update src/ ./dev_build_misc_static.sh
-run_on_update art_game/ ./dev_build_ag.sh
+run_on_update src/tutorials/starter_app/ ./dev_build_ag.sh
 if [ "$1" == "sync" ]; then
 	rebuild_on_update
 	cd build && browser-sync start -s -f . --port 9000 --reload-delay 10000 --reload-debounce 10000
--- install_docs.sh
+++ install_docs.sh
@@ -1,63 +1,32 @@
 #!/bin/bash
 
-FILES=$(find coreconcepts/* -maxdepth 0 -type f)
+./create_docs.sh
 
+cd docs
 
-for f in $FILES
-do
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'/'      # Change IFS to new line
-  fn=($f) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
-  length2=${#fn[@]}
-  file_name=${fn[$(expr $length2 - 1)]}
-  echo "cc $file_name"
-  cp $f docs/concepts/$file_name
-done
+source_md () {
+  FILES=$(find $1 -maxdepth 0 -type f)
+  for f in $FILES
+  do
+    SAVEIFS=$IFS   # Save current IFS
+    IFS=$'/'      # Change IFS to new line
+    fn=($f) # split to array $names
+    IFS=$SAVEIFS   # Restore IFS
+    length2=${#fn[@]}
+    file_name=${fn[$(expr $length2 - 1)]}
 
-FILES=$(find coreconcepts/tutorials/* -maxdepth 0 -type f)
+    SAVEIFS=$IFS   # Save current IFS
+    IFS=$'.'      # Change IFS to new line
+    changes=($f) # split to array $names
+    IFS=$SAVEIFS   # Restore IFS
+    length=${#changes[@]}
 
+    if [ "${changes[$(expr $length - 1)]}" = "md" ]; then
+      echo "cc tut $file_name"
+      single_source md $f $f
+    fi
+  done
+}
 
-for f in $FILES
-do
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'/'      # Change IFS to new line
-  fn=($f) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
-  length2=${#fn[@]}
-  file_name=${fn[$(expr $length2 - 1)]}
-
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'.'      # Change IFS to new line
-  changes=($f) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
-  length=${#changes[@]}
-
-  if [ "${changes[$(expr $length - 1)]}" = "md" ]; then
-    echo "cc tut $file_name"
-    single_source md $f docs/tutorials/coreconcepts/$file_name
-  fi
-done
-
-FILES=$(find art_game/* -maxdepth 0 -type f)
-
-for f in $FILES
-do
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'/'      # Change IFS to new line
-  fn=($f) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
-  length2=${#fn[@]}
-  file_name=${fn[$(expr $length2 - 1)]}
-
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'.'      # Change IFS to new line
-  changes=($f) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
-  length=${#changes[@]}
-
-  if [ "${changes[$(expr $length - 1)]}" = "md" ]; then
-    echo "Starter app $file_name"
-    single_source md $f docs/tutorials/starter_app/$file_name
-  fi
-done
+source_md tutorials/coreconcepts/*
+source_md tutorials/starter_app/*
