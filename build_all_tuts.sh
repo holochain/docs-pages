5871be8ef2a7e0642db66c0206dc1d52ab8b23fb
Tom Gowan <s3549186@student.rmit.edu.au>
2019-11-28 13:47:34 +1100

    change to netlify

---
+++ build_all_tuts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+single_source md src/tutorials/coreconcepts/hello_holo.md docs/tutorials/coreconcepts/hello_holo.md
+single_source md src/tutorials/coreconcepts/hello_test.md docs/tutorials/coreconcepts/hello_test.md
+single_source md src/tutorials/coreconcepts/hello_gui.md docs/tutorials/coreconcepts/hello_gui.md
+single_source md src/tutorials/coreconcepts/hello_me.md docs/tutorials/coreconcepts/hello_me.md
+single_source md src/tutorials/coreconcepts/hello_world.md docs/tutorials/coreconcepts/hello_world.md
+single_source md src/tutorials/coreconcepts/simple_micro_blog.md docs/tutorials/coreconcepts/simple_micro_blog.md
