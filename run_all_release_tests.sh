5436749986af8fe3e734de5bade2300271b8fc71
freesig <tomrgowan@gmail.com>
2019-12-21 16:12:48 +1100

    fix ci 2

--- run_all_release_tests.sh
+++ run_all_release_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cargo install single_source &&
 ./test_release.sh hello_holo && ./test_release.sh hello_test && ./test_release.sh hello_gui && ./test_release.sh hello_me && ./test_release.sh hello_world && ./test_release.sh simple_micro_blog
