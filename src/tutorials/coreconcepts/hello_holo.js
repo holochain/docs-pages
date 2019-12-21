2433934d3fb0f30d577a292a1e074f68084ffc6d
freesig <tomrgowan@gmail.com>
2019-12-21 16:21:39 +1100

    fix ci 3

--- src/tutorials/coreconcepts/hello_holo.js
+++ src/tutorials/coreconcepts/hello_holo.js
@@ -1,7 +1,11 @@
+/// NB: The try-o-rama config patterns are still not quite stabilized.
+/// See the try-o-rama README [https://github.com/holochain/try-o-rama]
+/// for a potentially more accurate example
+
 const path = require('path')
 const tape = require('tape')
 
-const { Diorama, tapeExecutor, backwardCompatibilityMiddleware } = require('@holochain/diorama')
+const { Orchestrator, Config, tapeExecutor, singleConductor, combine  } = require('@holochain/tryorama')
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
@@ -9,20 +13,45 @@ process.on('unhandledRejection', error => {
 });
 
 const dnaPath = path.join(__dirname, "../dist/cc_tuts.dna.json")
-const dna = Diorama.dna(dnaPath, 'cc_tuts')
 
-const diorama = new Diorama({
-  instances: {
-    alice: dna,
-    bob: dna,
+const orchestrator = new Orchestrator({
+  middleware: combine(
+    // squash all instances from all conductors down into a single conductor,
+    // for in-memory testing purposes.
+    // Remove this middleware for other "real" network types which can actually
+    // send messages across conductors
+    singleConductor,
+
+    // use the tape harness to run the tests, injects the tape API into each scenario
+    // as the second argument
+    tapeExecutor(require('tape'))
+  ),
+
+  globalConfig: {
+    logger: false,
+    network: {
+      type: 'sim2h',
+      sim2h_url: 'wss://localhost:9000',
+    },
   },
-  bridges: [],
-  debugLog: false,
-  executor: tapeExecutor(require('tape')),
-  middleware: backwardCompatibilityMiddleware,
+
+  // the following are optional:
+
+  waiter: {
+    softTimeout: 5000,
+    hardTimeout: 10000,
+  },
 })
 
-diorama.registerScenario("description of example test", async (s, t, { alice }) => {
+const config = {
+  instances: {
+    cc_tuts: Config.dna(dnaPath, 'cc_tuts'),
+  }
+}
+orchestrator.registerScenario('Test hello holo', async (s, t) => {
+  const {alice, bob} = await s.players({alice: config, bob: config}, true);
+  const result = await alice.call('cc_tuts', 'hello', 'hello_holo', {});
+  t.ok(result.Ok);
+  t.deepEqual(result, { Ok: 'Hello Holo' })
 })
-
-diorama.run()
+orchestrator.run()
