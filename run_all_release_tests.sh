fbfbd155ebd4b52feb54c5976cb3d9064644c989
freesig <tomrgowan@gmail.com>
2019-12-21 16:26:20 +1100

    fix ci 4

--- run_all_release_tests.sh
+++ run_all_release_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cargo install single_source &&
+sim2h_server &
 ./test_release.sh hello_holo && ./test_release.sh hello_test && ./test_release.sh hello_gui && ./test_release.sh hello_me && ./test_release.sh hello_world && ./test_release.sh simple_micro_blog
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sim2h_server &
 ./build_test_cc.sh hello_holo && ./build_test_cc.sh hello_test && ./build_test_cc.sh hello_gui && ./build_test_cc.sh hello_me && ./build_test_cc.sh hello_world && ./build_test_cc.sh simple_micro_blog
