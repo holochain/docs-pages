a267c272c51e2f80cbf01615a803ae63d569b25a
Paul d'Aoust <paul.daoust@holo.host>
2020-04-10 11:41:26 -0700

    missed one zome template update + whitespace fixes

--- src/tutorials/coreconcepts/hello_test.rs
+++ src/tutorials/coreconcepts/hello_test.rs
@@ -2,16 +2,19 @@
 
 use hdk::prelude::*;
 use hdk_proc_macros::zome;
+
 #[zome]
 mod hello_zome {
     #[init]
     fn init() {
         Ok(())
     }
+
     #[validate_agent]
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
         Ok(())
     }
+
     #[zome_fn("hc_public")]
     pub fn hello_holo() -> ZomeApiResult<String> {
         Ok("Hello Holo".into())
