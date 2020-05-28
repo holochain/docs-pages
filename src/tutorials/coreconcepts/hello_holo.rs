471a3421bbb242c051819593f209ab0e2c774232
Paul d'Aoust <paul.daoust@holo.host>
2020-05-27 20:57:59 -0700

    Revert "bump hc version references to 0.0.47"
    This reverts commit e9f49b49c383695d72e8e60c16d834dcb7a0dd21.

--- src/tutorials/coreconcepts/hello_holo.rs
+++ src/tutorials/coreconcepts/hello_holo.rs
@@ -3,7 +3,7 @@
 use hdk::prelude::*;
 use hdk_proc_macros::zome;
 
-// see https://developer.holochain.org/api/0.0.48-alpha1/hdk/ for info on using the hdk library
+// see https://developer.holochain.org/api/0.0.47-alpha1/hdk/ for info on using the hdk library
 
 // This is a sample zome that defines an entry type "MyEntry" that can be committed to the
 // agent's chain via the exposed function create_my_entry
