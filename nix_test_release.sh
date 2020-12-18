16936b00b1b4a893d2945b11d0373e1ee3900e1e
Paul d'Aoust <paul.daoust@holo.host>
2020-12-17 16:01:53 -0800

    lots of CI/build/deploy config changes
    remove all automated tests; simiplify stuff; will revisit automated testing for install page in the new year

--- nix_test_release.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nix-shell https://github.com/holochain/holonix/archive/${RELEASE_VERSION_ENV}.tar.gz --run './run_all_release_tests.sh' 
--- run_all_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sim2h_server &
-./build_test_cc.sh hello_holo && ./build_test_cc.sh hello_test && ./build_test_cc.sh hello_gui && ./build_test_cc.sh hello_me && ./build_test_cc.sh hello_world && ./build_test_cc.sh simple_micro_blog
