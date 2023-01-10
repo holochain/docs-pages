331344f08ffa20a940792876c8c965bae00be670
Matthew Nichols <matthew@matthew-nichols.com>
2023-01-10 09:20:53 +0000

    Added ability to add svg icon to linkTile

--- 11ty-extensions/card-shortcodes.js
+++ 11ty-extensions/card-shortcodes.js
@@ -1,10 +1,35 @@
+const fs = require('fs');
+const path = require("path");
+
 module.exports = function(eleventyConfig) {
-  
-  eleventyConfig.addPairedShortcode('linkTile', function(content, href, classStringAddition = null) {
-    const addedClassString = classStringAddition ? `link-tile ${classStringAddition}` : "link-tile";
+
+  /**
+   * Returns markup for a link tile card
+   * @param href The url to set the href of the link to
+   * @param classStringAddition (Optional) Class names to add to the card 
+   * @param iconPath (Optional) src relative path to the SVG icon file to insert into the head-space
+   */
+  eleventyConfig.addPairedShortcode('linkTile', function(content, href, classStringAddition = null, iconPath = "") {
+    const addedClassString = !!classStringAddition ? `link-tile ${classStringAddition}` : "link-tile";
     
+    let optionalSvgTag = "";
+    if (!!iconPath) {
+      if (path.extname(iconPath) != '.svg') {
+        throw new Error("linkTile shortcode can only render an svg icon");
+      }
+
+      let conditionedFilePath = iconPath;
+      if (iconPath.indexOf(".") !== 0) {
+        const srcDir = `${eleventyConfig.dir.input}/`
+        conditionedFilePath = `${srcDir}/${iconPath}`;
+      }
+
+      const svgData = fs.readFileSync(conditionedFilePath);
+      optionalSvgTag = svgData.toString();
+    }
+
     return `<a href="${href}" class="${addedClassString}">
-      <div class="head-space"></div>
+      <div class="head-space">${optionalSvgTag}</div>
       <div class="content-wrapper">${content}</div>
     </a>`;
   });
