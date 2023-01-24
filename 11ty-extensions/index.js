3b09bae9d0349bd6bff72319b9f3acb51ddba630
Matthew Nichols <matthew@matthew-nichols.com>
2023-01-24 17:52:53 +0000

    Adding a new nunjucks global to send a console log msg.

--- 11ty-extensions/index.js
+++ 11ty-extensions/index.js
@@ -1,19 +1,9 @@
 const LayoutBlockShortcodes = require("./layout-block-shortcodes");
 const CardShortcodes = require("./card-shortcodes");
+const NunjucksExt = require("./nunjucks-exts");
 
 module.exports = function(eleventyConfig) {
   LayoutBlockShortcodes(eleventyConfig);
   CardShortcodes(eleventyConfig);
-
-  eleventyConfig.addNunjucksGlobal("callMacroByName", function(name) {
-    if (!name) {
-      return;
-    }
-
-    const otherArgs = [...arguments].slice(1);
-    const member = this.ctx[name];
-    if (typeof member === "function") {
-      return member(...otherArgs);
-    }
-  })
+  NunjucksExt(eleventyConfig);
 }
