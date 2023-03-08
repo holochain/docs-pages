0b1f8b55569cea6266eda4b330e2799651eb542e
Matthew Nichols <matthew@matthew-nichols.com>
2023-03-08 02:47:24 +0000

    Adding the 1 second debounce to update url

--- src/client-side/page-search.ts
+++ src/client-side/page-search.ts
@@ -1,3 +1,17 @@
+const debounce = (callback: (...params: any[]) => any, waitTime: number) => {
+  let timeoutId: number | null = null;
+  
+  return (...args: any[]) => {
+    if (timeoutId) {
+      window.clearTimeout(timeoutId);
+    }
+    
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args);
+    }, waitTime);
+  };
+}
+
 /** Query string parameter name holding the current search */
 const PagefindSearchKey = "pagefind-search";
 
@@ -7,17 +21,21 @@ function setupPagefindUI() {
     showImages: false
   });
   
-  const searchInput = window.document.querySelector<HTMLInputElement>("#search .pagefind-ui__search-input");
+  const searchInput = window.document.querySelector<HTMLInputElement>("#search .pagefind-ui__search-input")!;
   const pagefindSearchQString = (new URL(window.location.toString())).searchParams.get(PagefindSearchKey);
   
   if (pagefindSearchQString) {
     pagefindUIInstance.triggerSearch(pagefindSearchQString);
   }
 
+  const debouncedInputHandler = debounce(() => updateSearchQueryString(searchInput?.value), 1000);
+
   // Clear the query string if it is empty
   searchInput?.addEventListener("input", (e: Event) => {
     if (searchInput.value === "") {
       updateSearchQueryString("");
+    } else {
+      debouncedInputHandler();
     }
   });
   
@@ -39,7 +57,7 @@ function setupPagefindUI() {
     const url = new URL(window.location.toString());
     if (term.trim() == "") {
       url.searchParams.delete(PagefindSearchKey);
-    } else {
+    } else if (url.searchParams.get(PagefindSearchKey) !== term) {
       url.searchParams.set(PagefindSearchKey, term);
     }
     history.pushState({}, "", url);
