9289f3cf9c026e878e0b41ab41adff35f088d78b
Matthew Nichols <matthew@matthew-nichols.com>
2023-03-21 13:16:31 +0000

    switched output path to the page context.

--- 11ty-extensions/eleventy-transforms.js
+++ 11ty-extensions/eleventy-transforms.js
@@ -3,10 +3,10 @@ const { noopener } = require("posthtml-noopener");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addTransform("addRelNoopener", async function(content, outputPath) {
+  eleventyConfig.addTransform("addRelNoopener", async function(content) {
     const noopenerInst = noopener();
 
-    if (outputPath.endsWith(".html")) {
+    if (this.page.outputPath.endsWith(".html")) {
       const result = await postHtml.use(noopenerInst).process(content);
       return result.html;
     }
