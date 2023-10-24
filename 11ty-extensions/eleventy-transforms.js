d5a2126c2585ec62c81530bbba153c42978243e3
Paul d'Aoust <paul.daoust@holo.host>
2023-10-24 09:57:28 -0700

    just load top-level highlightjs module

--- 11ty-extensions/eleventy-transforms.js
+++ 11ty-extensions/eleventy-transforms.js
@@ -1,7 +1,7 @@
 const postHtml = require("posthtml")();
 const htmlMin = require("html-minifier");
 const { noopener } = require("posthtml-noopener");
-const highlightjs = require("highlight.js/lib/common");
+const highlightjs = require("highlight.js");
 
 module.exports = function(eleventyConfig) {
 
