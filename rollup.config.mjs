ba7fada08109ee49dc9aa473311b95a2832162a5
Matt Gabrenya <matt@buildyourweb.app>
2024-01-08 12:00:34 -0700

    refactor: remove use of tsc, have rollup manage building ts, bundle mermaid js via node modules

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,40 @@
-export default {
-  input: './client-side-compiled/index.js',
-  output: {
-    file: '_site/scripts/index.bundled.js'
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default [
+  {
+    input: 'src/client-side/index.ts',
+    output: {
+      file: '_site/scripts/index.js',
+      sourcemap: '_site/scripts/index.js.map',
+      inlineDynamicImports: true,
+    },
+    plugins: [
+      typescript(),
+      nodeResolve({
+          browser: true
+        }
+      ),
+      commonjs({
+        include: /node_modules/,
+      }),
+    ],
+  },
+  {
+    input: 'src/client-side/page-search.ts',
+    output: {
+      file: '_site/scripts/page-search.js',
+      sourcemap: '_site/scripts/page-search.js.map',
+    },
+    plugins: [
+      typescript(),
+      nodeResolve({
+        browser: true
+      }),
+      commonjs({
+        include: /node_modules/,
+      })
+    ],
   }
-}
+]
