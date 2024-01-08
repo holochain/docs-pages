2dd0413f9e70537fde79ce166eefa049460998f2
Matt Gabrenya <matt@buildyourweb.app>
2024-01-08 13:38:45 -0700

    feat: minify all js bundles, rename output files to *.min.js

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,37 +7,38 @@ export default [
   {
     input: 'src/client-side/index.ts',
     output: {
-      file: '_site/scripts/index.js',
-      sourcemap: '_site/scripts/index.js.map',
+      file: '_site/scripts/index.min.js',
+      sourcemap: '_site/scripts/index.min.js.map',
       inlineDynamicImports: true,
     },
     plugins: [
       typescript(),
+      terser(),
     ],
   },
   {
     input: 'src/client-side/page-search.ts',
     output: {
-      file: '_site/scripts/page-search.js',
-      sourcemap: '_site/scripts/page-search.js.map',
+      file: '_site/scripts/page-search.min.js',
+      sourcemap: '_site/scripts/page-search.min.js.map',
     },
     plugins: [
-      typescript()
+      typescript(),
+      terser(),
     ],
   },
   {
     input: 'src/client-side/mermaid.ts',
     output: {
-      file: '_site/scripts/mermaid.js',
-      sourcemap: '_site/scripts/mermaid.js.map',
+      file: '_site/scripts/mermaid.min.js',
+      sourcemap: '_site/scripts/mermaid.min.js.map',
       inlineDynamicImports: true,
     },
     plugins: [
       typescript(),
       nodeResolve({
           browser: true
-        }
-      ),
+      }),
       commonjs({
         include: /node_modules/,
       }),
