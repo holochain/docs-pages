ad23306982c4a8caa11c0a020baef74ecf0cf334
Paul d'Aoust <paul.daoust@holo.host>
2025-05-22 14:24:21 -0700

    feat: only copy unchanged or added lines for diffs

--- src/client-side/index.ts
+++ src/client-side/index.ts
@@ -1,7 +1,26 @@
 function copyCodeBlockClickHandler(e: Event) {
   const button = e.target as HTMLButtonElement;
   const codeEl = button.parentElement?.querySelector("code");
-  navigator.clipboard.writeText(codeEl?.innerText || "")
+  if (!codeEl) { return; }
+  let codeToCopy = "";
+  if (codeEl.className.match(/\blanguage-diff/)) {
+    // If we've got a diff, only copy the unchanged or added lines;
+    // skip the deleted lines.
+    codeToCopy = codeEl.innerText
+      .split(/\r?\n/)
+      .reduce(
+        (acc, line) => {
+          if ([" ", "+"].includes(line.substring(0, 1))) {
+            acc += line.substring(1);
+          }
+          return acc;
+        },
+        ""
+      );
+  } else if (codeEl.innerText) {
+    codeToCopy = codeEl.innerText;
+  }
+  navigator.clipboard.writeText(codeToCopy);
 }
 
 function addCopyButtonsToCodeSections() {
